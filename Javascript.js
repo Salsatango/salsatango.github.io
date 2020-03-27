66988f481223cfb4b6fd3b9d0d39c0fb7a7d3e07
Salsatango <36876745+Salsatango@users.noreply.github.com>
2020-03-27 14:05:18 -0400

    Added some content and code

--- Javascript.js
+++ Javascript.js
@@ -1,1 +1,45 @@
+
+//Function that takes the cursor position (event)
+window.scrollTo(0,0);
+
+function registerCoords(event) {
+  	var x = event.pageX;
+  	var y = event.pageY;
+	/*
+	how to get the vertical scroll pixels of an element
+	var scrolled = document.getElementById("page");
+	var z = scrolled.scrollTop;
+	*/
+  document.getElementById("followCursor").style.left = (x-35) + "px";
+  document.getElementById("followCursor").style.top = y-35 + "px";
+
+}
+
+function stopFollowingCursor() {
+  document.getElementById("followCursor").style.opacity = "0" ;
+}
+
+function startFollowingCursor() {
+  document.getElementById("followCursor").style.opacity = "70";
+}
+
+
+
+
+
+/*Function to animate text, taken from https://tobiasahlin.com 
+It uses anime.js */
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2000,
+    delay: (el, i) => 50 * (i+1)
+  })
 // JavaScript Document
