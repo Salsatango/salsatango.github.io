c394a5dda93bae7ae07da85e2ae1b1296d15dc70
Salsatango <36876745+Salsatango@users.noreply.github.com>
2022-01-20 19:51:12 -0500

    fixed json file

--- javascript/portfolio2022.js
+++ javascript/portfolio2022.js
@@ -10,6 +10,24 @@ for (var y = 0; y < allSections.length;){
 	allSections[y].classList.add(y+1);
 	y++;
 }}
+
+// This is to load the JSON file
+
+async function getJSON() {
+    let url = 'https://jsonware.com/json/abfe005c41c8214e22e487b8d6eff417.json'
+    let obj = await (await fetch(url)).json();
+    
+    //console.log(obj);
+    return obj;
+}
+var tags;
+(async () => {
+  tags = await get()
+  //console.log(tags)
+  document.getElementById("tags").innerHTML = JSON.stringify(tags);
+})()
+
+
 //this function does what its name says it does
 function insecureConsoleLogs(){
 	console.log(numberOfEntries);
